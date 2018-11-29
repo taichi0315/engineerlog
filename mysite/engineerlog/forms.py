ddaaa934265e970c1aa9105d12e81043836f925c
taichi0315 <k.taichi0315@gmail.com>
2018-11-30 07:19:06 +0900

    before delete durationModel

--- mysite/engineerlog/forms.py
+++ mysite/engineerlog/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 from .models import AppUser, Post
-from django.forms.widgets import NumberInput, ClearableFileInput
-
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class LoginForm(AuthenticationForm):
@@ -13,9 +11,6 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = AppUser
         fields = ('displayname', 'icon', 'profile_sentence')
-        widgets = {
-            'icon': ClearableFileInput()
-        }
 
 class SignUpForm(UserCreationForm):
     
@@ -30,6 +25,3 @@ class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("duration", "comment")
-        widgets = {
-            'duration': NumberInput(attrs={'type':'range', 'step':'5'})
-        }
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'imagekit',
     'crispy_forms',
     'bootstrap4',
+    'material_widgets',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
